a8c629fe9a818846882405786d1951301f156c08
E335-comma <ifeoluwaaderoju6@gmail.com>
2026-03-16 13:32:53 +0100

    define backend block

--- terraform-config/provider.tf
+++ terraform-config/provider.tf
@@ -1,3 +1,12 @@
+terraform { 
+    backend "s3" {
+        bucket = "demo2-terraform-state-bucket"
+        key = "terraform.tfstate"
+        region = "eu-north-1"
+    }
+}
+
+
 provider "aws" {
     region = "eu-north-1"
 }
